03d688c850598f533009cf87b24e1c85b735e64c
Jimmy Devine <jimmy@jimmydevine.com>
2018-11-22 14:32:35 +0000

    update

--- action-tvoff.py
+++ action-tvoff.py
@@ -24,5 +24,5 @@ if __name__ == "__main__":
     lang = "EN"
     with Hermes(MQTT_ADDR.encode("ascii")) as h:
         h.skill = skill
-        h.subscribe_intent("tvoff", tvoff)
+        h.subscribe_intent("jiffe:tvoff", tvoff)
         h.loop_forever()
