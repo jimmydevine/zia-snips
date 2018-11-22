1837e80c5097cabc9c066a08665741f5730a92ce
Jimmy Devine <jimmy@jimmydevine.com>
2018-11-22 05:33:54 +0000

    tvoff

--- action-tvoff.py
+++ action-tvoff.py
@@ -7,7 +7,7 @@ class Skill(object):
 
 
 def tvoff(hermes, intent_message):
-    with open('/tmp/tvoff', 'w') as fp:
+    with open('/tmp/tvoff3', 'w') as fp:
         fp.close()
     current_session_id = intent_message.session_id
     hermes.publish_end_session(current_session_id, res.decode("latin-1"))
@@ -16,8 +16,13 @@ def tvoff(hermes, intent_message):
 if __name__ == "__main__":
     skill = Skill()
     
+    with open('/tmp/tvoff1', 'w') as fp:
+        fp.close()
+    
     lang = "EN"
     with Hermes(MQTT_ADDR.encode("ascii")) as h:
+        with open('/tmp/tvoff2', 'w') as fp:
+            fp.close()
         h.skill = skill
         h.subscribe_intent("tvoff", tvoff)
         h.loop_forever()
