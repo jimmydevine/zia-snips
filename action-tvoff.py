be5637fe23ceddc542aa61349b76395d357b2fbe
Jimmy Devine <jimmy@jimmydevine.com>
2018-11-22 14:18:12 +0000

    update

--- action-tvoff.py
+++ action-tvoff.py
@@ -2,31 +2,27 @@
 # -*-: coding utf-8 -*-
 from hermes_python.hermes import Hermes
 import hermes_python
-import io
-import os
-import sys
+
+
+MQTT_IP_ADDR = "localhost"
+MQTT_PORT = 1883
+MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
 class Skill(object):
     pass
 
 
 def tvoff(hermes, intent_message):
-    with open('/tmp/tvoff3', 'w') as fp:
-        fp.close()
     current_session_id = intent_message.session_id
     hermes.publish_end_session(current_session_id, res.decode("latin-1"))
 
 
 if __name__ == "__main__":
     skill = Skill()
-    
-    with open('/tmp/tvoff1', 'w') as fp:
-        fp.close()
+    print 'TVOFF STARTED'
     
     lang = "EN"
     with Hermes(MQTT_ADDR.encode("ascii")) as h:
-        with open('/tmp/tvoff2', 'w') as fp:
-            fp.close()
         h.skill = skill
         h.subscribe_intent("tvoff", tvoff)
         h.loop_forever()
