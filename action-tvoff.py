3a09f6bb53495dd8ac5d729e441d6dde981ee085
Jimmy Devine <jimmy@jimmydevine.com>
2018-11-22 04:53:55 +0000

    Init

---
+++ action-tvoff.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python2
+# -*-: coding utf-8 -*-
+
+
+class Skill(object):
+    pass
+
+
+def tvoff(hermes, intent_message):
+    current_session_id = intent_message.session_id
+    hermes.publish_end_session(current_session_id, res.decode("latin-1"))
+
+
+if __name__ == "__main__":
+    skill = Skill()
+    
+    lang = "EN"
+    with Hermes(MQTT_ADDR.encode("ascii")) as h:
+        h.skill = skill
+        h.subscribe_intent("tvoff",
+                           tvoff) \
+        .loop_forever()
